07e493b344d8c2a546225152f5a70b6597a68c6a
Faopaw <66549556+Faopaw@users.noreply.github.com>
2023-01-26 09:30:14 +0000

    style changes

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -2,3 +2,14 @@ import { render, screen } from "@testing-library/react";
 import Homepage from "../pages/index";
 import "@testing-library/jest-dom";
 
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Homepage />);
+
+    const heading = screen.getByRole("heading", {
+      name: /welcome to next\.js!/i,
+    });
+
+    expect(heading).not.toBeInTheDocument();
+  });
+});
